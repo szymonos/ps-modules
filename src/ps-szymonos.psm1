08aa366ab74e8c7e76f0907f81a32da99d17272c
Szymon Osiecki <szymonos@outlook.com>
2022-05-15 23:17:06 +0200

    refactor: module files split

--- src/ps-szymonos.psm1
+++ src/ps-szymonos.psm1
@@ -1,9 +1,12 @@
-. $PSScriptRoot/Functions.ps1
+. $PSScriptRoot/Functions/common.ps1
+. $PSScriptRoot/Functions/az.ps1
 
 $exportModuleMemberParams = @{
     Function = @(
+        # common
         'Get-ArrayIndexMenu',
         'Invoke-CommandRetry',
+        # az
         'Connect-AzContext',
         'Set-SubscriptionMenu',
         'Get-AzAccessToken',
