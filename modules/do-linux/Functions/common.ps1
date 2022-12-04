360d13540ef9860b3354164e0f2c52fbec7776e1
Szymon Osiecki <szymonos@outlook.com>
2022-12-04 11:17:02 +0100

    feat: do-linux v0.1.0
    Add sudo functions

--- modules/do-linux/Functions/common.ps1
+++ modules/do-linux/Functions/common.ps1
@@ -1,5 +1,48 @@
 <#
 .SYNOPSIS
+Run commands as root in PowerShell.
+.DESCRIPTION
+Wrapper for sudo command to handle defined aliases and one-liner functions.
+#>
+function Invoke-Sudo {
+    for ($i = 0; $i -lt $args.Count; $i++) {
+        # expand arguments alias/function definition
+        if ($cmd = (Get-ChildItem Alias:/$($args[$i]) -ErrorAction SilentlyContinue || Get-ChildItem Function:/$($args[$i]) -ErrorAction SilentlyContinue).Definition.Where({ $_ -notmatch '\n' })) {
+            $args[$i] = "$cmd".Trim().Replace('$input | ', '').Replace('& /usr/bin/env ', '').Replace(' @args', '')
+        } elseif ($args[$i] -match ' ') {
+            # quote arguments with spaces
+            $args[$i] = "'$($args[$i])'"
+        }
+    }
+    & /usr/bin/env bash -c "/usr/bin/env sudo $args"
+}
+
+Set-Alias -Name _ -Value Invoke-Sudo
+
+<#
+.SYNOPSIS
+Run PowerShell cmdlets as root.
+.DESCRIPTION
+Wrapper for sudo command to execute PowerShell cmdlets and handle defined aliases and one-liner functions.
+#>
+function Invoke-SudoPS {
+    for ($i = 0; $i -lt $args.Count; $i++) {
+        # expand arguments alias/function definition
+        if ($cmd = (Get-ChildItem Alias:/$($args[$i]) -ErrorAction SilentlyContinue || Get-ChildItem Function:/$($args[$i]) -ErrorAction SilentlyContinue).Definition.Where({ $_ -notmatch '\n' })) {
+            $args[$i] = "$cmd".Trim().Replace('$input | ', '').Replace('& /usr/bin/env ', '').Replace(' @args', '')
+        } elseif ($args[$i] -match ' ') {
+            # quote arguments with spaces
+            $args[$i] = "'$($args[$i])'"
+        }
+    }
+    # run sudo command with resolved commands
+    & /usr/bin/env sudo $params pwsh -NoProfile -NonInteractive -Command "$args"
+}
+
+Set-Alias -Name sps -Value Invoke-SudoPS
+
+<#
+.SYNOPSIS
 Fix executable bit based on shebang presence.
 .DESCRIPTION
 Process specified directory, looking for bash and powershell scripts, and sets executable bit based on shebang presence.
