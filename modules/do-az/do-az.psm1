4830e9e0a3e87628c3c9e0e1b2cbf28f7598d95f
Szymon Osiecki <szymonos@outlook.com>
2022-12-04 16:13:04 +0100

    refactor: do-az v1.10.0
    required do-common module
    remove common functions

--- modules/do-az/do-az.psm1
+++ modules/do-az/do-az.psm1
@@ -1,13 +1,8 @@
-. $PSScriptRoot/Functions/common.ps1
 . $PSScriptRoot/Functions/az.ps1
 . $PSScriptRoot/Functions/azgraph.ps1
 
 $exportModuleMemberParams = @{
     Function = @(
-        # common
-        'Invoke-CommandRetry'
-        'Get-ArrayIndexMenu'
-        'ConvertFrom-CIDR'
         # az
         'Connect-AzContext'
         'Set-SubscriptionMenu'
