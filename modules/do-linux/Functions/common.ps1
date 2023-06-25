ee826ae56d3423da56c714481c55981f287983d0
Szymon Osiecki <szymonos@outlook.com>
2023-06-25 22:35:34 +0200

    refactor: do-linux v0.4.10
    Invoke-ExecutableBitFix - add .py extension to script filters.

--- modules/do-linux/Functions/common.ps1
+++ modules/do-linux/Functions/common.ps1
@@ -98,41 +98,37 @@ function Invoke-ExecutableBitFix {
         [Parameter(Position = 0)]
         [ValidateNotNullorEmpty()]
         [ValidateScript({ Test-Path $_ -PathType 'Container' }, ErrorMessage = "`e[1;4m{0}`e[22;24m is not valid path")]
-        [string]$Path = '.'
+        [string]$Path = '.',
+
+        [ValidateNotNullorEmpty()]
+        [string[]]$ExtensionFilter = @('.ps1', '.py', '.sh')
     )
 
-    begin {
-        # script extensions
-        [string[]]$extFilter = @('.ps1', '.sh')
-    }
-
-    process {
-        # *adding executable bit in files with shebang
+    # *adding executable bit in files with shebang
         (Get-ChildItem $Path -File -Recurse -Force).Where({
-                $_.DirectoryName -notmatch '/\.git\b' `
-                    -and ($_.Extension -in $extFilter -or -not $_.Extension) `
-                    -and $_.UnixMode -notmatch '^-rwx' `
-                    -and (Get-Content $_ -Head 1 | Select-String '^#!' -Quiet)
-            }
-        ).ForEach({
-                Write-Host $_.FullName -ForegroundColor Green
-                chmod +x $_.FullName
-            }
-        )
+            $_.DirectoryName -notmatch '/\.git\b' `
+                -and ($_.Extension -in $ExtensionFilter -or -not $_.Extension) `
+                -and $_.UnixMode -notmatch '^-rwx' `
+                -and (Get-Content $_ -Head 1 | Select-String '^#!' -Quiet)
+        }
+    ).ForEach({
+            Write-Host $_.FullName -ForegroundColor Green
+            chmod +x $_.FullName
+        }
+    )
 
-        # *removing executable bit from files without shebang
+    # *removing executable bit from files without shebang
         (Get-ChildItem $Path -File -Recurse -Force).Where({
-                $_.DirectoryName -notmatch '/\.git\b' `
-                    -and ($_.Extension -in $extFilter -or -not $_.Extension) `
-                    -and $_.UnixMode -match '^-rwx' `
-                    -and (Get-Content $_ -Head 1 | Select-String '^#!' -NotMatch -Quiet)
-            }
-        ).ForEach({
-                Write-Host $_.FullName
-                chmod -x $_.FullName
-            }
-        )
-    }
+            $_.DirectoryName -notmatch '/\.git\b' `
+                -and ($_.Extension -in $ExtensionFilter -or -not $_.Extension) `
+                -and $_.UnixMode -match '^-rwx' `
+                -and (Get-Content $_ -Head 1 | Select-String '^#!' -NotMatch -Quiet)
+        }
+    ).ForEach({
+            Write-Host $_.FullName
+            chmod -x $_.FullName
+        }
+    )
 }
 
 Set-Alias -Name fxmod -Value Invoke-ExecutableBitFix
